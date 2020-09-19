5ac290bab3032bc6478dc91d67cdc05352203f1a
qudan7 <qudan7@gmail.com>
2020-09-19 22:57:24 +0900

    Test

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,85 @@
+#include "Lab2.h"
+
+using namespace std;
+
+namespace lab2
+{
+    void PrintIntegers(std::istream& in, std::ostream& out)
+    {
+        int input;
+        string trash;
+
+        cout << setw(12) << "oct" << setw(11) << "dec" << setw(9) << "hex" << endl << endl;
+
+        while (!in.eof()) {
+
+            in >> input;
+
+            if (!in.fail())
+            {
+                cout << setw(12) << oct << input << setw(11) << dec << input << setw(9) << hex << uppercase << input << endl;
+            }
+            else
+            {
+                in.clear();
+                in >> trash;
+            }
+        }
+    }
+
+
+    void PrintMaxFloat(std::istream& in, std::ostream& out)
+    {
+        float input;
+        float max = LLONG_MIN;
+        string trash;
+
+        cout << fixed << setprecision(3);
+
+        while (!in.eof()) {
+
+            in >> input;
+
+            if (!in.fail())
+            {
+                if (input < 0) 
+                {
+                    cout << setw(6) << '-';
+                    input = -input;
+                }
+                else
+                {
+                    cout << setw(6) << '+';
+                }
+
+                cout << setw(14) << input << endl;
+
+                if (max < input)
+                {
+
+                    max = input;
+
+                }
+            }
+            else
+            {
+                in.clear();
+                in >> trash;
+            }
+
+        }
+
+        if (max < 0)
+        {
+            cout << "max:" << setw(6) << '-';
+            max = -max;
+        }
+        else
+        {
+            cout << "max:" << setw(6) << '+';
+        }
+
+        cout << setw(14) << max << endl;
+
+    }
+}
