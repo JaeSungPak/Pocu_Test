409305439b81d25390b9dfdb8a691212d4a60baa
qudan7 <qudan7@gmail.com>
2020-09-25 14:50:28 +0900

    no message

--- Lab3/TimeSheet.cpp
+++ Lab3/TimeSheet.cpp
@@ -5,36 +5,73 @@ namespace lab3
 {
 	TimeSheet::TimeSheet() : mTimeLocate(0), mName("")
 	{
+		mTime = NULL;
 	}
 	TimeSheet::TimeSheet(const char* name, unsigned int maxEntries) : mTimeLocate(0), mName(name)
 	{
+		mTime = new int[maxEntries];
 	}
 	TimeSheet::TimeSheet(const TimeSheet& other)
 	{
+		mTimeLocate = other.mTimeLocate;
+		mName = other.mName;
+		mTime = new int[_msize(other.mTime) / 4];
+		memcpy(mTime, other.mTime, _msize(other.mTime));
 	}
 
 	void TimeSheet::AddTime(int timeInHours)
 	{
 
-		
+		if (timeInHours >= 1 && timeInHours <= 10 && static_cast<unsigned>(mTimeLocate) < _msize(mTime) / sizeof(int))
+		{
+			mTime[mTimeLocate] = timeInHours;
+			mTimeLocate += 1;
+		}
+
 	}
 	int TimeSheet::GetTimeEntry(unsigned int index) const
 	{
-		return 0;
+
+		if (index <= static_cast<unsigned int>(mTimeLocate) && index >= 0)
+		{
+			return mTime[index];
+		}
+
+		return -1;
 	}
 
 	int TimeSheet::GetTotalTime() const
 	{
-		return 0;
+
+		int total = 0;
+
+		for (int i = 0; i < mTimeLocate; i++)
+		{
+			total += mTime[i];
+		}
+
+		return total;
 	}
 
 	float TimeSheet::GetAverageTime() const
 	{
-		return 0;
+		float average = static_cast<float>(GetTotalTime()) / mTimeLocate;
+
+		return average;
 	}
 	float TimeSheet::GetStandardDeviation() const
 	{
-		return 0;
+
+		float average = GetAverageTime();
+		float temp = 0;
+
+		for (int i = 0; i < mTimeLocate; i++)
+		{
+			float minus = static_cast<float>(mTime[i]);
+			temp += (minus - average) * (minus - average);
+		}
+
+		return static_cast<float>(sqrt(temp / static_cast<float>(mTimeLocate)));
 	}
 	const std::string& TimeSheet::GetName() const
 	{
@@ -46,6 +83,17 @@ namespace lab3
 	}
 	TimeSheet& TimeSheet::operator= (const TimeSheet& other)
 	{
+
+		if (reinterpret_cast<int>(this) == reinterpret_cast<int>(&other))
+		{
+			return *this;
+		}
+		mTimeLocate = other.mTimeLocate;
+		mName = other.mName;
+		delete[] mTime;
+		mTime = new int[_msize(other.mTime) / 4];
+		memcpy(mTime, other.mTime, _msize(other.mTime));
+
 		return *this;
 	}
 
