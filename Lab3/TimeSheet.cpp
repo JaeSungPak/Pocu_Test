3ba857d912444bec60e3461be321416d5a9785cc
qudan7 <qudan7@gmail.com>
2020-09-25 15:01:47 +0900

    no message

--- Lab3/TimeSheet.cpp
+++ Lab3/TimeSheet.cpp
@@ -13,65 +13,30 @@ namespace lab3
 	}
 	TimeSheet::TimeSheet(const TimeSheet& other)
 	{
-		mTimeLocate = other.mTimeLocate;
-		mName = other.mName;
-		mTime = new int[_msize(other.mTime) / 4];
-		memcpy(mTime, other.mTime, _msize(other.mTime));
 	}
 
 	void TimeSheet::AddTime(int timeInHours)
 	{
 
-		if (timeInHours >= 1 && timeInHours <= 10 && static_cast<unsigned>(mTimeLocate) < _msize(mTime) / sizeof(int))
-		{
-			mTime[mTimeLocate] = timeInHours;
-			mTimeLocate += 1;
-		}
-
+		
 	}
 	int TimeSheet::GetTimeEntry(unsigned int index) const
 	{
-
-		if (index <= static_cast<unsigned int>(mTimeLocate) && index >= 0)
-		{
-			return mTime[index];
-		}
-
-		return -1;
+		return 0;
 	}
 
 	int TimeSheet::GetTotalTime() const
 	{
-
-		int total = 0;
-
-		for (int i = 0; i < mTimeLocate; i++)
-		{
-			total += mTime[i];
-		}
-
-		return total;
+		return 0;
 	}
 
 	float TimeSheet::GetAverageTime() const
 	{
-		float average = static_cast<float>(GetTotalTime()) / mTimeLocate;
-
-		return average;
+		return 0;
 	}
 	float TimeSheet::GetStandardDeviation() const
 	{
-
-		float average = GetAverageTime();
-		float temp = 0;
-
-		for (int i = 0; i < mTimeLocate; i++)
-		{
-			float minus = static_cast<float>(mTime[i]);
-			temp += (minus - average) * (minus - average);
-		}
-
-		return static_cast<float>(sqrt(temp / static_cast<float>(mTimeLocate)));
+		return 0;
 	}
 	const std::string& TimeSheet::GetName() const
 	{
@@ -83,17 +48,6 @@ namespace lab3
 	}
 	TimeSheet& TimeSheet::operator= (const TimeSheet& other)
 	{
-
-		if (reinterpret_cast<int>(this) == reinterpret_cast<int>(&other))
-		{
-			return *this;
-		}
-		mTimeLocate = other.mTimeLocate;
-		mName = other.mName;
-		delete[] mTime;
-		mTime = new int[_msize(other.mTime) / 4];
-		memcpy(mTime, other.mTime, _msize(other.mTime));
-
 		return *this;
 	}
 
