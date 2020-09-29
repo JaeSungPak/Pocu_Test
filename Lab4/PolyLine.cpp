911a75be93a30afc368bfc81d7c51f843498bffd
qudan7 <qudan7@gmail.com>
2020-09-29 17:25:43 +0900

    no message

--- Lab4/PolyLine.cpp
+++ Lab4/PolyLine.cpp
@@ -26,15 +26,6 @@ namespace lab4
 
 	bool PolyLine::AddPoint(float x, float y)
 	{
-		if (mLocate < 10)
-		{
-			mLine[mLocate] = Point(x, y);
-
-			mLocate += 1;
-
-			return true;
-		}
-
 		return false;
 	}
 
