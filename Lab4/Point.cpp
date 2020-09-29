02f9fa487f6925c6925e7bda70a7298531b20f7c
qudan7 <qudan7@gmail.com>
2020-09-29 15:22:00 +0900

    no message

---
+++ Lab4/Point.cpp
@@ -0,0 +1,70 @@
+#include "Point.h"
+
+namespace lab4
+{
+	Point::Point() : mX(0), mY(0)
+	{
+	}
+
+	Point::Point(float x, float y) : mX(x), mY(y)
+	{
+	}
+
+	Point::~Point()
+	{
+	}
+
+	Point& Point::operator=(const Point& other)
+	{
+		mX = other.mX;
+		mY = other.mY;
+
+		return *this;
+	}
+
+	Point Point::operator+(const Point& other) const
+	{
+		Point point(mX + other.mX, mY + other.mY);
+
+		return point;
+	}
+
+	bool Point::operator==(const Point& other) const
+	{
+		return (mX == other.mX && mY == other.mY) ? true : false;
+	}
+
+	Point Point::operator-(const Point& other) const
+	{
+		Point point(mX - other.mX, mY - other.mY);
+
+		return point;
+	}
+
+	float Point::Dot(const Point& other) const
+	{
+		return (mX * other.mX) + (mY * other.mY);
+	}
+
+	Point Point::operator*(float operand) const
+	{
+		Point point(mX * operand, mY * operand);
+
+		return point;
+	}
+
+	Point operator*(float operand, const Point& point)
+	{
+		return point * operand;
+	}
+
+	float Point::GetX() const
+	{
+		return mX;
+	}
+
+	float Point::GetY() const
+	{
+		return mY;
+	}
+}
---
+++ Lab4/Point.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace lab4
+{
+	class Point
+	{
+	public:
+		Point();
+		Point(float x, float y);
+		~Point();
+
+		Point operator+(const Point& other) const;
+		Point operator-(const Point& other) const;
+		float Dot(const Point& other) const;
+		Point operator*(float operand) const;
+		Point& operator=(const Point& other);
+		friend Point operator*(float operand, const Point& point);
+		bool operator==(const Point& other) const;
+
+		float GetX() const;
+		float GetY() const;
+		
+	private:
+		float mX;
+		float mY;
+	};
+}
